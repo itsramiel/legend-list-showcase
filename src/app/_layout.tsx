4ad7e3ea7b875153d11d3a142aff853979b5f9fe
Rami Elwan <ramielwan48@gmail.com>
2025-05-03 09:24:12 +0200

    feat: reorganize project

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,21 +1,12 @@
-import "../global.css";
+import "@/global.css";
 
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import "react-native-reanimated";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <>
       <Stack>
         <Stack.Screen
           name="index"
@@ -31,6 +22,6 @@ export default function RootLayout() {
         />
       </Stack>
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </>
   );
 }
